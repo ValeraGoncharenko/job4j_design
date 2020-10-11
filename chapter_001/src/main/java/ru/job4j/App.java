c2c3c7d8dfe247ba44e59d953f6bfffc8e877eaa
Goncharenko Valery <goncharikvv@gmail.com>
2020-10-11 23:45:31 +0300

    0. Инструменты [#383461]

---
+++ chapter_001/src/main/java/ru/job4j/App.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    @SuppressWarnings("checkstyle:MethodParamPad")
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
